e1a303c6ab380e53198799c529a0a9f94bbebbcd
Hummingbed <sniyokratos@gmail.com>
2021-03-16 09:43:32 +0100

    Food ordering webpage with dashboard

---
+++ FeyFood_Complete_Ecommerce/partials-front/footer.php
@@ -0,0 +1,14 @@
+<!-- social Section Starts Here -->
+
+ 
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved</p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
